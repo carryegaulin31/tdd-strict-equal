dae271364d0048bdccaa0669197e28e4b6e51690
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-16 18:58:57 -0400

    error fix

--- calculate.js
+++ calculate.js
@@ -1,5 +1,5 @@
 const strictEqual = (num1, num2) => {
-  return (typeof num1 == typeof num2) && (num1 == num2)
+  return (typeof num1 === typeof num2) && (num1 === num2)
 }
 
 module.exports = strictEqual
